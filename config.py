1087c56e77a4ada84f7a6c0d0d27deaf89216a2a
AbidAbdullah <161892101+AbidAbdullah199@users.noreply.github.com>
2024-04-02 12:31:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first} Claim or File By After Joining my Channal")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Claim your File After Joined my Channal")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
